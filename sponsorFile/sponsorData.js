bfe60756b20da81581ffb410e02b364e7ff65d6c
unknown <simplecarrier101@gmail.com>
2022-12-22 22:36:10 -0800

    committing the data from api cal

--- sponsorFile/sponsorData.js
+++ sponsorFile/sponsorData.js
@@ -1,49 +1,57 @@
 
 
-// we going to create a function that builds the card elements
 
-function createChildCard(image, childName) {
+    // we going to create a function that builds the card elements
 
-
-    // create Element
-const mainDiv = document.createElement("div")
-const childImage = document.createlement("img")
-const childFullName = document.createElement("P")
-
-// add the data 
-childImage.src = image
-childFullName.textContent = childName
-
-// append elements  together 
-
-mainDiv.appendChild("childImage")
-mainDiv.appendChild("childfulName")
+    function createChildCard(image, childName) {
 
 
-return mainDiv
+        // create Element
+    const mainDiv = document.createElement("div")
+    const childImage = document.createElement("img")
+    const childFullName = document.createElement("P")
+
+    // add the data 
+    childImage.src = image
+    childFullName.textContent = childName
+
+    // add classes
+
+    mainDiv.classList.add("child-card")
+    childImage.classList.add("child-image")
+
+    // append elements  together 
+
+    mainDiv.appendChild(childImage)
+    mainDiv.appendChild(childFullName)
+
+
+    return mainDiv
 
 
 
-
-}
-
-
-const entary = document.querySelector(".entary")
+    }
 
 
-axios.get("https://phpstack-776148-3099296.cloudwaysapps.com/public/api/orphans")
-.than(response => {
- response.data.foreac(child =>{
- const name = child.name
- const image = child.image.url 
+    const entary = document.querySelector(".entary")
 
- entary.appendChild(createChildCard(image, childName))
 
- })
+    axios.get("https://phpstack-776148-3099296.cloudwaysapps.com/public/api/orphans")
+    .then(response => {
+    response.data.map(child => {
 
-})
-.catch(error =>{
+      
 
-console.log(error)
+    const name = child.name
+    const image = child.image_url 
 
-})
+    entary.appendChild(createChildCard(image,name))
+
+    })
+
+    })
+    .catch(error =>{
+
+    console.log(error)
+
+    })
